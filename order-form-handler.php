f94749eb003bd39a53d6a182c82a156e1f4d5e61
Cristian Louie Agustin <60724087+shi4no@users.noreply.github.com>
2020-12-06 21:06:04 -1000

    Add files via upload

---
+++ order-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+  $name = $_Post['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'ianagustin0913@gmail.com';
+  
+  $email_subject = "New Form Submission";
+
+  $email_body = "User Name: $name.\n".
+                 "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+  
+  $to = "cristianlouie@gmial.com";
+  
+  $headers = "From: $email_from \r\n";
+ 
+  $headers = "Reply-To: $visitor_email \r\n";
+  
+  mail($to,$email_subject,$email_body,$headers);
+
+ header("Location: order.html");
+
+
+?>
